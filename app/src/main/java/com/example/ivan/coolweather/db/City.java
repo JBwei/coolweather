5f923d96ea35a81b6f323f51ecc70fd0d559e215
Ivan <937329641@qq.com>
2017-03-31 12:21:49 +0800

    添加数据库和表的各项配置

---
+++ app/src/main/java/com/example/ivan/coolweather/db/City.java
@@ -0,0 +1,53 @@
+package com.example.ivan.coolweather.db;
+
+/**
+ * Created by Ivan on 2017/3/31.
+ */
+
+public class City
+{
+	private int id;
+	private String cityName;
+	private int cityCode;
+	private int provinceId;
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public String getCityName()
+	{
+		return cityName;
+	}
+	
+	public void setCityName(String cityName)
+	{
+		this.cityName = cityName;
+	}
+	
+	public int getCityCode()
+	{
+		return cityCode;
+	}
+	
+	public void setCityCode(int cityCode)
+	{
+		this.cityCode = cityCode;
+	}
+	
+	public int getProvinceId()
+	{
+		return provinceId;
+	}
+	
+	public void setProvinceId(int provinceId)
+	{
+		this.provinceId = provinceId;
+	}
+}
